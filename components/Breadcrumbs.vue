6a786200e65aed43738029012d18068a3b9959a8
Don <donshius@inbox.lt>
2020-06-05 22:20:52 +0100

    add breadcrums component

---
+++ components/Breadcrumbs.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul v-if="crumbs" class="uk-breadcrumb">
+        <li><n-link to="/">Home</n-link></li>
+        <li v-for="(crumb, idx) in crumbs" :key="idx">
+            <n-link v-if="crumb.path" :to="crumb.path">{{ crumb.name }}</n-link>
+            <span v-else>{{ crumb.name }}</span>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: ['crumbs']
+}
+</script>
+
+<style scoped>
+.uk-breadcrumb {
+    margin-bottom: 10px;
+}
+</style>
---
+++ pages/auth/signup.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="uk-container uk-margin-bottom">
+        <Breadcrumbs :crumbs="crumbs" />
+        <h3 class="page-title">Registration</h3>
+        <div class="uk-box-shadow-medium">
+            Signup form
+        </div>
+    </div>
+</template>
+
+<script>
+import Breadcrumbs from '../../components/Breadcrumbs'
+
+export default {
+    components: { Breadcrumbs },
+    data() {
+        return {
+            crumbs: [
+                {
+                    name: 'Sign up'
+                }
+            ]
+        }
+    }
+}
+</script>
